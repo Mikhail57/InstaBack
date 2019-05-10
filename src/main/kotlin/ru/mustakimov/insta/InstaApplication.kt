388bc7251253be9287bde53331b720392df32349
Mikhail Mustakimo <mikhail@mustakimov.ru>
2019-05-11 01:59:39 +0800

    CrossOrigin

--- src/main/kotlin/ru/mustakimov/insta/InstaApplication.kt
+++ src/main/kotlin/ru/mustakimov/insta/InstaApplication.kt
@@ -3,10 +3,22 @@ package ru.mustakimov.insta
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @EnableCaching
-class InstaApplication
+class InstaApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("*")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<InstaApplication>(*args)
